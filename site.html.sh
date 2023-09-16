a863e2c3f763a3fce6fc6ac26c979bef9285f11f
lf94 <inbox@leefallat.ca>
2023-09-15 20:54:44 -0400

    Update the site

--- site.html.sh
+++ site.html.sh
@@ -4,18 +4,32 @@ echo "
 <head>
   <title>backlight-auto</title>
   <style>
-    h1, p, div, body { font-size: 1.1em; font-family: monospace; }
+    body { max-width: 80ch; margin: auto; padding: 1em 1ch; }
+    p, div, body { font-size: 1.1em; font-family: monospace; }
+    body, a { background-color: black; color: lightgrey; }
+    h1, h2, h3 { text-align: center; }
   </style>
 </head>
 <body>
   <h1>backlight-auto</h1>
+  <h3>The missing backlight auto adjusting program for Linux.</h3>
   <p>Tired of adjusting brightness going from place to place?</p>
   <p>Has the sun gone down and not realize you're burning out your eyes?</p>
   <p>Want to adjust certain monitor's brightness using certain video sources?</p>
   <p>Looking for a tool that has 2 dependencies and gets the job done?</p>
   <p>backlight-auto is here to help.</p>
-  <a href="$(echo "data:application/zip;base64,$(zip -R - $(git ls-files) | base64 -w 0)")">Download</a>
-  <hr/>
+  <h2>Installation</h2>
+  <p>Unfortunately backlight-auto is not yet packaged for distributions. If you're a package manager
+  I'd love to help you package this.</p>
+  <p>The first step is to install Zig.</p>
+  <p>The second step is to install libyuv and video4linux2 headers.</p>
+  <p>For both of those steps, please use a search engine. The instructions change all the time.</p>
+  <p>Third, save this embedded zip (you've already downloaded it if you're viewing this page):</p>
+  <center>
+    <a href=\"$(echo "data:application/zip;base64,$(zip -R - $(git ls-files) | base64 -w 0)")\">download.zip</a>
+  </center>
+  <p>Last, build the project with \`zig build\` and copy \`zig-out/bin/backlight-auto\` to your preferred place.</p>
+  <h2>Usage</h2>
   <p>There are multiple ways to setup using backlight-auto, but the first step
   is to tell the program what your webcam sees as \"black\".</p>
   <p>To start, place your thumb over your webcam.</p>
@@ -25,5 +39,9 @@ echo "
   <p>You'll need to adjust the permissions of your backlight files or run the program as super user.</p>
   <p>To run it periodically, create a systemd file.</p>
   <p>I personally assign it to a function key and run it when needed.</p>
+  <h2>Contributions</h2>
+  <p>Email len@falken.directory with suggestions or patches.</p><
+  <p>This tool is small, with the aim to be naturally forked and maintained forever.</p>
+  <p>This software is evolutionary. The strongest fork is the official fork.</p>
 </body>
 ";
